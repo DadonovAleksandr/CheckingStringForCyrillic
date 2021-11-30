a004099db887fdf6c80452d6208584a9869c4b3a
DadonovAD <DadonovAD@tvv.transneft.ru>
2021-11-30 14:33:38 +0300

    Убрал кириллические символы

--- CheckingStringForCyrillic/Program.cs
+++ CheckingStringForCyrillic/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Unicode;
 
 namespace CheckingStringForCyrillic
 {
@@ -6,7 +10,27 @@ namespace CheckingStringForCyrillic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] files = { "TspdBlockCode.txt", "TspdDataTypeCode.txt", "TspdGroupParamCode.txt", "TspdInstalPlaceCode.txt", "TspdParamCode.txt", "TspdTouCode.txt" };
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+                List<string> list = File.ReadLines(file).ToList();
+                list.RemoveAll(item => item.Contains("Description"));
+                for(int i=0;i<list.Count;i++)
+                {
+                    string[] parts = list[i].Split("//");
+                    list[i] = parts[0].Trim().Replace(",", "");
+                }
+                
+                foreach (var item in list)
+                {
+                    var res = item.Any(ch => ch >= UnicodeRanges.Cyrillic.FirstCodePoint && ch  < (UnicodeRanges.Cyrillic.FirstCodePoint + UnicodeRanges.Cyrillic.Length));
+                    if(res)
+                        Console.WriteLine(item);
+                }    
+            }
+            
         }
     }
 }
